2214ff68c39837fa260354f45d78ae7fcb34dd41
nasim-alamdari <nasim.alamdari@gmail.com>
2023-03-10 15:49:12 -0800

    add files

---
+++ Code/func/audio_record.py
@@ -0,0 +1,109 @@
+
+"""
+installation on Mac M1:
+1. brew install portaudio
+2. python -m pip install --global-option='build_ext' --global-option='-I/opt/homebrew/Cellar/portaudio/19.7.0/include' --global-option='-L/opt/homebrew/Cellar/portaudio/19.7.0/lib' pyaudio
+"""
+
+import os
+import pyaudio
+#import torchaudio
+import numpy as np
+from scipy.io.wavfile import write
+import struct
+import IPython
+    
+def record (
+    duration: int,
+    record_name: str,
+    record_save_path: os.PathLike,
+    ):
+
+    #=====================================
+    # Open an audio stream from the microphone with PyAudio
+
+    INPUT_BLOCK_TIME = 0.0125
+    sample_rate = 16000
+    CHUNK = int(sample_rate*INPUT_BLOCK_TIME) # FRAMES_PER_BUFFER
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    SHORT_NORMALIZE = (1.0/32768.0)
+
+    p = pyaudio.PyAudio()
+
+    #=======================================
+
+    def _find_input_device():
+        device_index = None            
+        for i in range( p.get_device_count() ):     
+            devinfo = p.get_device_info_by_index(i)   
+            print( "Device %d: %s"%(i,devinfo["name"]) )
+
+            for record_name in ["mic","input"]:
+                if record_name in devinfo["name"].lower():
+                    print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
+                    device_index = i
+                    return device_index
+
+        if device_index == None:
+            print( "No preferred input found; using default input device." )
+
+        return device_index
+
+    device_index = _find_input_device()
+
+    #=======================================
+    """player = p.openstream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=sample_rate,
+        output=True,
+        frames_per_buffer=CHUNK,
+    )"""
+    # starts recording
+    stream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=sample_rate,
+        input=True,
+        input_device_index = device_index,
+        frames_per_buffer=CHUNK,
+    )
+
+    print("Start recording...")
+    frames = []
+    cnt = 0
+
+    for i in range(int(duration*sample_rate/CHUNK)): #do this for 10 seconds
+        # to play back
+        #player.write(np.frombuffer(stream.read(CHUNK),dtype=np.int16),CHUNK)
+        #block = np.frombuffer(stream.read(CHUNK),dtype=np.int16)
+        block = stream.read(CHUNK)
+        count = len(block)/2
+        format = "%dh"%(count)
+        shorts = struct.unpack( format, block )
+        shorts = np.int16(shorts)
+        frames.append(np.squeeze(shorts[:]))
+
+        if cnt==0:
+            print(len(block), count, format, len(shorts))
+
+        if cnt%200==0:
+            print("...")
+        cnt+=1
+    
+    #=====================================
+    wav = np.asarray(frames).flatten()
+    
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    write(os.path.join(record_save_path,record_name),  sample_rate, wav)
+    print("Recording Completed")
+
+
+#if __name__ == "__main__":
+#    record (record_name: str,record_save_path: os.PathLike)
+
+
+
---
+++ Code/func/spk_segment.py
@@ -0,0 +1,91 @@
+"""
+# for speechbrain
+!pip install -qq torch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 torchtext==0.12.0
+!pip install -qq speechbrain==0.5.12
+
+# pyannote.audio
+!pip install -qq pyannote.audio
+
+# for visualization purposes
+!pip install -qq ipython==7.34.0
+"""
+
+# Call speaker-segmentation model from pyannote:
+from pyannote.audio import Pipeline
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import numpy as np
+import sox
+import os
+from pathlib import Path
+import subprocess
+
+def segment (
+    keyword: str,
+    recording_path: os.PathLike):
+    
+    #keyword = "heynasim"
+    #recording_path = './content/target_kw/recording/heynasim/'
+
+    wav_path = os.path.join(recording_path, keyword+'.wav')
+    fs, wav = wavfile.read(wav_path)
+    
+    pipeline = Pipeline.from_pretrained('pyannote/speaker-segmentation',
+                        use_auth_token='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')
+    output = pipeline(wav_path)
+    
+    def _plot_out(wav, output):
+        fs, wav = wavfile.read(wav_path)
+        plt.figure(figsize = (10,3))
+        plt.plot(wav)
+        ymax = max(wav)
+
+        for turn, _, speaker in output.itertracks(yield_label=True):
+            # speaker speaks between turn.start and turn.end
+            #print(turn.end- turn.start )
+            plt.plot([ turn.start*fs, turn.end*fs - 1], [ymax * 1.1, ymax * 1.1], color = 'orange')
+
+        plt.xlabel('sample')
+        plt.grid()
+        
+    #_plot_out(wav,output)    
+    extractions = os.path.join(recording_path, "extractions")
+    if not os.path.exists(extractions):
+        os.makedirs(extractions)
+
+    #Convert wavform to correct format in WAV:
+    #!ffmpeg -hide_banner -loglevel error -y -i wav_path -acodec pcm_s16le -ac 1 -ar 16000 wav_path
+    subprocess.run('ffmpeg -hide_banner -loglevel error -y -i wav_path -acodec pcm_s16le -ac 1 -ar 16000 wav_path', shell=True)
+    
+    cnt = 0
+    for turn, _, speaker in output.itertracks(yield_label=True):
+        # speaker speaks between turn.start and turn.end
+        cnt+=1
+        time_ms_st = turn.start*fs # convert to sec
+        time_ms_en = turn.end*fs # convert to sec
+
+
+        turn_st_flr = np.round(turn.start*1000)/1000
+        dest_wav = os.path.join(extractions,str(f"{cnt:03d}_{keyword}_detection_{turn_st_flr}sec.wav"))
+
+        # make sure audio segment is 1sec
+        if (time_ms_en - time_ms_st +1) < fs:
+            append_len_ms = fs - (time_ms_en - time_ms_st +1)
+            append_len_s =  append_len_ms/fs # convert to sec
+        print(turn.start, turn.end, append_len_s)
+
+        transformer = sox.Transformer()
+        transformer.convert(samplerate=16000)  
+        transformer.trim(turn.start, turn.end + append_len_s)
+        transformer.build(wav_path, dest_wav)
+    print("Speaker Segmentation Completed.")
+    return extractions
+
+
+    
+
+#if __name__ == "__main__":
+#    extractions = segment (keyword: str,recording_path: os.PathLike)
+
+
+
