43afa96881a735fe9775cb268fc8460a2f98d53c
nasim-alamdari <nasim.alamdari@gmail.com>
2023-03-28 00:54:28 -0700

    add streamlit and fastapit code for ec2

---
+++ Code/main_ec2.py
@@ -0,0 +1,98 @@
+# uvicorn main_ec2:app --reload --workers 1 --host 0.0.0.0 --port 8000
+import io
+import librosa
+import numpy as np
+from fastapi import FastAPI, File, Form
+from fastapi.responses import JSONResponse
+import os
+from pathlib import Path
+from model_realtime import preproc, train, predict, report_result, infer_rltime
+
+app = FastAPI()
+
+
+def proc_train(audio_data):
+    # Load audio data using librosa
+    audio, fs = librosa.load(io.BytesIO(audio_data), sr=None, mono=True)
+    
+    # segment the recorded audio
+    spk_segments_path = preproc (KEYWORD,keyword_dir,fs)
+    print('segmentation completed!')
+
+    test_samples= train(KEYWORD, spk_segments_path)
+    target_pred, nontarget_pred = predict(KEYWORD, test_samples)
+    frr_val,far_val = report_result (target_pred, nontarget_pred)
+    print('Training completed!')
+    print("FRR and FAA are:", frr_val, far_val)
+    return [frr_val, far_val]
+    
+
+
+# Define prediction function
+def eval_stream (KEYWORD, model,frames):
+    model_settings = input_data.standard_microspeech_model_settings(label_count=3)
+
+    test_spectrograms = input_data.to_micro_spectrogram(model_settings, frames)
+    test_spectrograms = test_spectrograms[np.newaxis, :, :, np.newaxis]
+
+    # fetch softmax predictions from the finetuned model:
+    pred = model.predict(test_spectrograms)
+    categorical_pred = np.argmax(pred, axis=1)
+    return pred,categorical_pred
+
+
+def predict_per_chunk(audio_data):
+    # Load audio data using librosa
+    audio, fs = librosa.load(io.BytesIO(audio_data), sr=None, mono=True)
+
+    # Define frame size and overlap
+    frame_size = fs # equivalend to 1-sec audio chunk
+    overlap = 0.5 # 50% overlap
+
+    # Calculate hop length based on overlap
+    hop_length = int(frame_size * overlap)
+
+    # Generate frames with overlap using a sliding Hann window approach to generate overlapping frames
+    #frames = librosa.util.frame(audio, frame_length=frame_size, hop_length=hop_length)
+    window = librosa.filters.get_window('hann', frame_size, fftbins=True)
+    frames = librosa.util.windows.sliding_window_view(audio, window.shape[0], hop_length=hop_length)
+
+    # Iterate over frames and process them as needed
+    preds = []
+    for frame in frames:
+        # Predict each 1-sec audio chunk
+        audio_chunk = np.frombuffer(frame, dtype=np.int16)
+        model, ths = infer_rltime(KEYWORD) # TODO: this should load only once
+        pred, categorical_pred = eval_stream (KEYWORD, model,audio_chunk)
+        if categorical_pred == 1:
+            if pred[0][categorical_pred] >= ths[0]:
+                preds.append("Other Words")
+        elif categorical_pred == 2:
+            if pred[0][categorical_pred] >= (ths[1]):
+                preds.append( "KEYWORD")
+        elif categorical_pred == 0:
+            if pred[0][categorical_pred] >= ths[2]:
+                preds.append( "Background Noise/Silence")
+        
+    return preds.tolist()
+
+
+@app.post("/train")
+async def make_prediction(file: bytes = File(...)):
+    # Make prediction using audio file data
+    result = proc_train(file)
+    # Return prediction result as JSON
+    return JSONResponse(content={"result": result})
+
+
+@app.post("/predict")
+async def make_prediction(file: bytes = File(...)):
+    # Make prediction using audio file data
+    predictions = predict_per_chunk(file)
+    # Return prediction result as JSON
+    return JSONResponse(content={"prediction": predictions})
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(main_ec2, host="0.0.0.0", port=8000)
---
+++ Code/streamlit_ec2.py
@@ -0,0 +1,141 @@
+# run this command: streamlit run streamlit_ec2.py
+import streamlit as st
+import requests
+import pyaudio
+import os
+from pathlib import Path
+from model_realtime import record, preproc, train, predict, report_result, infer
+#import websockets
+#import asyncio
+#import base64
+#import json
+import librosa
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+
+
+# Web user interface
+st.title('🎙️ Multilingual Custom Few-Shot Keyword Spotting App')
+
+with st.expander('About this App'):
+    st.markdown('''
+    This Streamlit app performs demo of training and streaming evaluation of custom keyword spotting through streamlit and fastapi model serving.
+
+    Steps:
+    - `1. Type a custom keyword without any space` - e.g. "heyGPT"
+    - `2. Upload you audio file for fine-tuning model.` - For security purposes, audio file will be deleted immediately after fine-tuning the model.
+    - `3. Upload you audio file for prediction` - Prediction has 3 classes: (1)keyword, (2)otherword, or (3)background noise/silence
+
+    ''')
+
+# Step 1: Define custom keyword
+st.markdown('##### :violet[Step 1: Enter Your Custom Keyword]')
+KEYWORD = st.text_input('e.g. heyGPT')
+st.write('The current Custom Keyword is: ', KEYWORD)
+
+def send_audio_file(file, flag):
+    # Define FastAPI endpoint URL
+    if flag == "train":
+        api_url = "http://0.0.0.0:8000/train"
+        files = {"file": (KEYWORD +"_audio_train.wav", file.read(), "audio/wav")}
+        st.write('Training may take few minutes...⌛')
+    elif flag == "predict":
+        api_url = "http://0.0.0.0:8000/predict"
+        files = {"file": (KEYWORD +"_audio_predict.wav", file.read(), "audio/wav")}
+        st.write('Prediction may take a minute...⌛')
+        
+    # Send POST request to FastAPI app
+    response = requests.post(api_url, files=files)
+    # Check if request was successful
+    if response.status_code == 200:
+        # Get prediction result from response
+        result = response.json()
+        # Display prediction result in Streamlit app
+        if flag == "predict":
+            st.write("Prediction result:", result)
+        return result
+    else:
+        st.write("Error making result request")
+        return None
+
+
+
+def upload_train_file():
+    keyword_dir = os.path.join(BASE_DIR , './content/target_kw/uploaded/',KEYWORD )
+    if not os.path.exists(keyword_dir):
+        os.mkdir(keyword_dir)
+        
+    uploaded_wav = st.file_uploader("Upload your (.wav) audio file", type=["wav"], key="audio_file1")
+
+    if uploaded_wav is not None:
+        st.markdown('###### :green[Upload completed! 🏁]')     
+        # Display the uploaded file
+        st.write(uploaded_wav)
+        
+        # Read WAV file and Display audio waveform
+        y, sr = librosa.load(uploaded_wav)
+        st.audio(y, format='audio/wav', sample_rate=sr)
+
+        uploaded_wav.name = KEYWORD + "_train_audio.wav" # rename audio before sending              
+        file_path = os.path.join(keyword_dir, uploaded_wav.name)
+        st.success("File saved successfully!")
+
+        # Create a button that the user must click after uploading the file
+        st.markdown('##### :violet[Step 3: Fine-Tuning the KWS Model]')
+        if st.button("Process Audio for Fine-Tuning"):
+            # Do something with the uploaded file
+            result = send_audio_file(uploaded_wav, "train")
+            st.write("Train File processed!")
+    else:
+        st.write("file is not uploaded yet.")
+        
+
+def upload_predict_file():
+    keyword_dir = os.path.join(BASE_DIR , './content/target_kw/uploaded/',KEYWORD )
+    if not os.path.exists(keyword_dir):
+        os.mkdir(keyword_dir)
+        
+    uploaded_wav = st.file_uploader("Upload your (.wav) audio file", type=["wav"], key="audio_file2")
+
+    if uploaded_wav is not None:
+        st.markdown('###### :green[Upload completed! 🏁]')     
+        # Display the uploaded file
+        st.write(uploaded_wav)
+        
+        # Read WAV file and Display audio waveform
+        y, sr = librosa.load(uploaded_wav)
+        st.audio(y, format='audio/wav', sample_rate=sr)
+
+        uploaded_wav.name = KEYWORD + "_predict_audio.wav" # rename audio before sending           
+        file_path = os.path.join(keyword_dir, uploaded_wav.name)
+        st.success("File saved successfully!")
+
+        # Create a button that the user must click after uploading the file
+        st.markdown('##### :violet[Step 5: Prediction]')
+        if st.button("Process Audio for Prediction"):
+            # Do something with the uploaded file
+            result = send_audio_file(uploaded_wav, "predict")
+            st.write("Predict File processed!")
+    else:
+        st.write("file is not uploaded yet.")
+
+        
+st.markdown('##### :violet[Step 2: Uploading a file for training]')
+upload_train_file()
+
+
+st.markdown('##### :violet[Step 4: Uploading a file for prediction]')       
+upload_predict_file()
+
+
+
+
+
+
+
+
+
+
+
+
+
